Bugs fixed and confusion matrix computation added

Conv1dNetwork.py
@@ -22,7 +22,7 @@ class Conv1dNetwork(nn.Module):
             else:
                 self.output_neurons = 2
         else:
-            self.layer_dims = [self.in_channels, 16, 32, 64]
+            self.layer_dims = [self.in_channels, 64, 64, 64]
             self.hidden_dim = 128
             self.num_rnn_layers = 1
             self.output_neurons = num_classes
@@ -37,6 +37,7 @@ class Conv1dNetwork(nn.Module):
             self.__dict__["pool" + str(i)] = nn.MaxPool1d(kernel_size=2)
             self.__dict__["relu" + str(i)] = nn.ReLU()
             self.__dict__["batch_norm" + str(i)] = nn.BatchNorm1d(self.layer_dims[i + 1])
+            self.__dict__["dropout" + str(i)] = nn.Dropout1d(p=0.1)
 
         if self.num_classes == 2:
             self.rnn = nn.RNN(input_size=self.layer_dims[-1], hidden_size=self.hidden_dim,
@@ -84,6 +85,8 @@ class Conv1dNetwork(nn.Module):
                 out = self.__dict__["pool" + str(i)](out)
             out = self.__dict__["relu" + str(i)](out)
             out = self.__dict__["batch_norm" + str(i)](out)
+            if self.num_classes > 2:
+                out = self.__dict__["dropout" + str(i)](out)
 
         if self.is_2d:
             out = torch.mean(out, dim=2)

Conv2dNetwork.py
@@ -16,8 +16,8 @@ class Conv2dNetwork(Conv1dNetwork):
             self.hidden_dim = 64
             self.num_rnn_layers = 1
         else:
-            self.layer_dims = [1, 16, 32, 64]
-            self.hidden_dim = 128
+            self.layer_dims = [1, 64, 64, 64]
+            self.hidden_dim = 64
             self.num_rnn_layers = 1
         self.num_conv_layers = len(self.layer_dims) - 1
 
@@ -29,6 +29,7 @@ class Conv2dNetwork(Conv1dNetwork):
                                                                kernel_size=3, stride=1, padding=1)
             self.__dict__["pool" + str(i)] = nn.MaxPool2d(kernel_size=(2, 2))
             self.__dict__["batch_norm" + str(i)] = nn.BatchNorm2d(self.layer_dims[i + 1])
+            self.__dict__["dropout" + str(i)] = nn.Dropout2d(p=0.1)
 
         if self.num_classes == 2:
             self.rnn = nn.RNN(input_size=self.layer_dims[-1], hidden_size=self.hidden_dim,

JAISystem.py
@@ -65,13 +65,17 @@ class JAISystem:
                 map = JAISystem.normalize_map(map)
             else:
                 if static_joints:
-                    map = np.mean(map, axis=0, keepdims=True)
+                    map = np.mean(map, axis=0)
                     map = JAISystem.normalize_map(map)
 
         if map is not None and not is_1d and not to_2d:
-            # Average map values among different axis of the same joint
-            joint_map = [np.mean(map[:, (i * 3):(i + 1) * 3], axis=1) for i in range(map.shape[1] // 3)]
-            map = np.stack(joint_map, 1)
+            if not static_joints:
+                # Average map values among different axis of the same joint
+                joint_map = [np.mean(map[:, (i * 3):(i + 1) * 3], axis=1) for i in range(map.shape[1] // 3)]
+                map = np.stack(joint_map, 1)
+            else:
+                # Average map values among different axis of the same joint
+                map = [np.mean(map[(i * 3):(i + 1) * 3]) for i in range(map.shape[0] // 3)]
 
             # Adjust color range
             map = JAISystem.normalize_map(map)
@@ -99,10 +103,13 @@ class JAISystem:
 
             # Plot joints
             if map is None or is_1d:
-                color = "g"
+                color = "gray"
                 cmap = None
             else:
-                color = map[frame]
+                if not static_joints:
+                    color = map[frame]
+                else:
+                    color = map
                 cmap = "jet"
 
             if not to_2d:
@@ -118,7 +125,7 @@ class JAISystem:
                 joint1_pos = (frame_x[connection[0]], frame_y[connection[0]], frame_z[connection[0]])
                 joint2_pos = (frame_x[connection[1]], frame_y[connection[1]], frame_z[connection[1]])
                 ax.plot([joint1_pos[0], joint2_pos[0]], [joint1_pos[1], joint2_pos[1]], [joint1_pos[2], joint2_pos[2]],
-                        c="r")
+                        c="gray")
 
             if to_2d:
                 # Draw joints of the same color
@@ -140,7 +147,7 @@ class JAISystem:
         return x, y
 
     def display_output(self, item_name, target_layer, target_class, x, y, explainer_type, map, output_prob,
-                       switch_map_format=False, show=False):
+                       switch_map_format=False, static_joints=False, show=False):
         title = "CAM for class " + str(target_class) + " (" + str(np.round(output_prob * 100, 2)) + "%) - true label: "\
                 + str(int(y))
         if not show:
@@ -152,7 +159,8 @@ class JAISystem:
                 "_" + str(target_class) + ".png", format="png", bbox_inches="tight", pad_inches=0, dpi=300)
             plt.close()
         else:
-            self.show_skeleton(item=x, map=map, title=title, switch_map_format=switch_map_format)
+            self.show_skeleton(item=x, map=map, title=title, switch_map_format=switch_map_format,
+                               static_joints=static_joints)
 
     @staticmethod
     def adjust_map(map, x, is_2d=True):

NetworkTrainer.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import random
 import numpy as np
 import keras
+import time
 from silence_tensorflow import silence_tensorflow
 from tcn import TCN
 
@@ -36,6 +37,8 @@ class NetworkTrainer(Trainer):
         self.train_dim = len(self.train_data)
         self.test_dim = len(self.test_data)
         self.net_type = net_type
+        self.start_time = None
+        self.end_time = None
 
         self.num_classes = len(self.train_data.classes)
         self.binary_output = binary_output
@@ -101,6 +104,9 @@ class NetworkTrainer(Trainer):
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
 
     def train(self, filename=None, show_epochs=False):
+        if show_epochs:
+            self.start_time = time.time()
+
         if self.use_cuda:
             net = self.net.cuda()
             self.criterion = self.criterion.cuda()
@@ -157,7 +163,12 @@ class NetworkTrainer(Trainer):
         self.net = net
         Trainer.save_model(self, filename, use_keras=use_keras)
 
-    def test(self, set_type=SetType.TRAINING):
+        if show_epochs:
+            self.end_time = time.time()
+            duration = self.end_time - self.start_time
+            print("Execution time:", round(duration / 60, 4), "min")
+
+    def test(self, set_type=SetType.TRAINING, show_cm=False):
         if self.use_cuda:
             net = self.net.cuda()
             self.criterion = self.criterion.cuda()
@@ -171,17 +182,17 @@ class NetworkTrainer(Trainer):
             data = self.test_data
             dim = self.test_dim
 
+        # Store class labels
+        class_labels = data.classes
+
         if self.normalize_input:
             data = SkeletonDataset.normalize_data(data, self.attr_mean, self.attr_std)
 
+        y_true = []
+        y_pred = []
         if self.net_type not in self.keras_networks:
             net.eval()
             loss = 0
-            acc = 0
-            tp = 0
-            tn = 0
-            fp = 0
-            fn = 0
             with torch.no_grad():
                 for x, y in data:
                     x = torch.from_numpy(x)
@@ -206,17 +217,17 @@ class NetworkTrainer(Trainer):
                         prediction = (output >= 0.5).float()
                     else:
                         prediction = np.argmax(output)
-                    acc += (prediction == y).item()
 
-                    # Confusion matrix definition
-                    cm = Trainer.compute_confusion_matrix(y, prediction)
-                    tp += cm[0]
-                    tn += cm[1]
-                    fp += cm[2]
-                    fn += cm[3]
+                    # Store values for Confusion Matrix calculation
+                    y_true.append(y)
+                    y_pred.append(prediction)
+
+            y_true = np.asarray(y_true)
+            y_pred = np.asarray(y_pred)
 
             loss /= dim
-            acc /= dim
+            acc = np.sum(y_true == y_pred) / dim
+
         else:
             x, y = data
             prediction = net.predict(x)
@@ -227,13 +238,26 @@ class NetworkTrainer(Trainer):
                 prediction = np.argmax(prediction, axis=1)
 
             loss, acc = net.evaluate(x, y)
-            cm = Trainer.compute_confusion_matrix(y, prediction)
-            tp = cm[0]
-            tn = cm[1]
-            fp = cm[2]
-            fn = cm[3]
 
+            # Store values for Confusion Matrix calculation
+            y_true = y
+            y_pred = prediction
+
+        cm = Trainer.compute_binary_confusion_matrix(y_true, y_pred)
+        tp = cm[0]
+        tn = cm[1]
+        fp = cm[2]
+        fn = cm[3]
         stats_holder = StatsHolder(loss, acc, tp, tn, fp, fn)
+
+        # Compute multiclass confusion matrix
+        cm_name = set_type.value + "_cm"
+        if show_cm:
+            img_path = self.results_dir + cm_name + ".png"
+        else:
+            img_path = None
+        self.__dict__[cm_name] = Trainer.compute_multiclass_confusion_matrix(y_true, y_pred, class_labels, img_path)
+
         return stats_holder
 
     def show_model(self):
@@ -277,14 +301,15 @@ if __name__ == "__main__":
                                  data_names=train_data1.remaining_instances)
 
     # Define the model
-    folder_name1 = "patientVSrandom_division_conv1d_15classes"
-    model_name1 = "conv1d_15classes"
+    folder_name1 = "models_for_JAI"
+    model_name1 = "conv2d_15classes"
     net_type1 = NetType.CONV2D
     binary_output1 = False
     normalize_input1 = True
-    lr1 = 0.001  # 0.01
+    # lr1 = 0.01
+    lr1 = 0.001
     trainer1 = NetworkTrainer(net_type=net_type1, working_dir=working_dir1, folder_name=folder_name1,
-                              train_data=train_data1, test_data=test_data1, epochs=100, lr=lr1,
+                              train_data=train_data1, test_data=test_data1, epochs=300, lr=lr1,
                               binary_output=binary_output1, normalize_input=normalize_input1)
 
     # Train the model
@@ -296,4 +321,5 @@ if __name__ == "__main__":
     use_keras1 = False
     trainer1 = Trainer.load_model(working_dir=working_dir1, folder_name=folder_name1, model_name=model_name1,
                                   use_keras=use_keras1)
-    trainer1.summarize_performance(show_process=True)
+    show_cm1 = True
+    trainer1.summarize_performance(show_process=True, show_cm=show_cm1)

Simulator.py
@@ -52,6 +52,10 @@ class Simulator:
         self.mean_test_stats = None
         self.dev_train_stats = None
         self.dev_test_stats = None
+        self.train_cm_list = []
+        self.train_cm_avg = None
+        self.test_cm_list = []
+        self.test_cm_avg = None
 
         # Create a folder to store models
         if folder_name not in os.listdir(working_dir + self.results_fold):
@@ -87,11 +91,22 @@ class Simulator:
             # Store results
             self.store_model_results(trainer)
 
+        # Compute average confusion matrix
+        self.train_cm_avg = np.mean(self.train_cm_list, axis=0)
+        Trainer.draw_multiclass_confusion_matrix(self.train_cm_avg, self.desired_classes,
+                                                 self.results_dir + "train_cm.png")
+        self.test_cm_avg = np.mean(self.test_cm_list, axis=0)
+        Trainer.draw_multiclass_confusion_matrix(self.test_cm_avg, self.desired_classes,
+                                                 self.results_dir + "test_cm.png")
+
     def store_model_results(self, trainer):
-        train_stats = trainer.test(set_type=SetType.TRAINING)
+        train_stats = trainer.test(set_type=SetType.TRAINING, show_cm=False)
         self.train_stats.append(train_stats)
-        test_stats = trainer.test(set_type=SetType.TEST)
+        self.train_cm_list.append(trainer.train_cm)
+
+        test_stats = trainer.test(set_type=SetType.TEST, show_cm=False)
         self.test_stats.append(test_stats)
+        self.test_cm_list.append(trainer.test_cm)
 
     def reload_simulation_results(self):
         for file in os.listdir(self.results_dir + self.simulator_name):

Trainer.py
@@ -6,11 +6,13 @@ import torch
 import dill
 import keras
 import os
+from torcheval.metrics.functional import multiclass_confusion_matrix
 
 from SetType import SetType
 from StatsHolder import StatsHolder
 from NetType import NetType
 from TCNNetwork import TCNNetwork
+from SkeletonDataset import SkeletonDataset
 
 
 # Class
@@ -31,17 +33,17 @@ class Trainer:
         self.train_data = train_data
         self.test_data = test_data
 
-    def test(self, set_type=SetType.TRAINING):
+    def test(self, set_type=SetType.TRAINING, show_cm=False):
         print("The model has not been defined.")
         train_stats = StatsHolder(float("inf"), 0, 0, 0, 0, 0)
         return train_stats
 
-    def summarize_performance(self, show_process=False):
+    def summarize_performance(self, show_process=False, show_cm=False):
         # Show final losses
-        train_stats = self.test(set_type=SetType.TRAINING)
+        train_stats = self.test(set_type=SetType.TRAINING, show_cm=show_cm)
         print("Train loss = " + str(round(train_stats.loss, 5)) + " - Train accuracy = " + str(round(train_stats.acc *
                                                                                                      100, 7)) + "%")
-        test_stats = self.test(set_type=SetType.TEST)
+        test_stats = self.test(set_type=SetType.TEST, show_cm=show_cm)
         print("Test loss = " + str(round(test_stats.loss, 5)) + " - Test accuracy = " + str(round(test_stats.acc * 100,
                                                                                                   7)) + "%")
 
@@ -55,7 +57,7 @@ class Trainer:
             plt.show()
 
     @staticmethod
-    def compute_confusion_matrix(y_true, y_predicted, classes=None):
+    def compute_binary_confusion_matrix(y_true, y_predicted, classes=None):
         if classes is None:
             # Classical binary computation (class 0 as negative and class 1 as positive)
             tp = (y_predicted == 1) & (y_true == 1)
@@ -75,12 +77,36 @@ class Trainer:
             for c in classes:
                 y_true_i = (y_true == c).astype(int)
                 y_predicted_i = (y_predicted == c).astype(int)
-                out_i = Trainer.compute_confusion_matrix(y_true_i, y_predicted_i, classes=None)
+                out_i = Trainer.compute_binary_confusion_matrix(y_true_i, y_predicted_i, classes=None)
                 out.append(out_i)
 
             out = np.asarray(out)
             out = [out[:, i] for i in range(out.shape[1])]
             return out
+
+    @staticmethod
+    def compute_multiclass_confusion_matrix(y_true, y_pred, classes, img_path=None):
+        # Compute confusion matrix
+        y_true = torch.tensor(y_true)
+        y_pred = torch.tensor(y_pred)
+        cm = multiclass_confusion_matrix(y_pred, y_true, len(classes), normalize="pred")
+
+        # Draw heatmap
+        if img_path is not None:
+            Trainer.draw_multiclass_confusion_matrix(cm, classes, img_path)
+
+        return cm
+
+    @staticmethod
+    def draw_multiclass_confusion_matrix(cm, classes, img_path):
+        plt.figure(figsize=(10, 10))
+        labels = [" ".join(SkeletonDataset.actions[c - 1].split(" ")[:2]) for c in classes]
+
+        plt.imshow(cm, cmap="jet")
+        plt.xticks(range(len(classes)), labels, rotation=45)
+        plt.yticks(range(len(classes)), labels, rotation=45)
+        plt.savefig(img_path, dpi=300)
+        plt.close()
 
     @staticmethod
     def save_model(trainer, model_name, use_keras,